9c46b593c48a9b2cb3ef1f5eebc277c1722afdde
niten <niten@fudo.org>
2025-12-11 10:39:47 -0800

    Output logs to console

--- src/tunabrain/__main__.py
+++ src/tunabrain/__main__.py
@@ -1,10 +1,15 @@
 from __future__ import annotations
 
+import logging
 import uvicorn
 
 from tunabrain.app import app
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s %(levelname)s [%(name)s] %(message)s",
+    )
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
